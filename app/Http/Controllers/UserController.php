48320893e98694f9c8ed29a8edcaf26568e966ee
JoseADAW <1898288@alu.murciaeduca.es>
2022-11-02 17:00:40 +0100

    siguiente paso

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $title = 'Listado de usuarios';
+
+        $users = [
+            'Joel',
+            'Ellie',
+            'Tess',
+            'Tommy',
+            'Bill',
+        ];
+
+        return view('users', compact(
+                'title',
+                'users'
+            )
+        );
+    }
+
+    public function show($id)
+    {
+        return 'Mostrando detalles del usuario: ' . $id;
+    }
+
+    public function create()
+    {
+        return 'Creando nuevo usuario';
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('usuarios', 'UserController@index');
+
+Route::get('usuarios/{id}', 'UserController@show')->where('id', '[0-9]+');
+
+Route::get('usuarios/nuevo', 'UserController@create');
+
+Route::get('saludo/{name}/{nickname?}', 'WelcomeUserController');
