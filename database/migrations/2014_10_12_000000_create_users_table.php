b865469e536d76d907566c718153ac88cb8d70a8
JoseADAW <1898288@alu.murciaeduca.es>
2022-11-04 20:23:20 +0100

    paso 11

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,6 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->string('profession',100);
             $table->rememberToken();
             $table->timestamps();
         });
