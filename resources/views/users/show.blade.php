d12f2a53e0002fec329d34666f42e7c9527d5d1a
JoseADAW <1898288@alu.murciaeduca.es>
2022-11-16 19:52:27 +0100

    paso 23

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -7,4 +7,7 @@
 
     <p>Mostrando los detalles del usuario {{ $user->name }}</p>
     <p>Correo electronico {{ $user->email }}</p>
+    <p>
+        <a href="{{route(('users'))}}">Regresar al listado de usuarios</a>
+    </p>
 @endsection
