720126209c440c60245abfcd4f20de97a80d4f58
JoseADAW <1898288@alu.murciaeduca.es>
2022-11-22 19:56:14 +0100

    paso 30

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -5,17 +5,41 @@
 @section('content')
     <h1>Crear nuevo usuario</h1>
 
+    @if($errors->any())
+        <div class="alert alert-danger">
+            <h6>Por favor, corrige los siguientes errores:</h6>
+            <ul>
+                @foreach($errors->all() as $error)
+                    <li>{{$error}}</li>
+                @endforeach
+            </ul>
+        </div>
+
+    @endif
+
     <form action="{{route('user.store')}}" method="post">
         {{csrf_field()}}
 
         <label for="name">Nombre:</label>
-        <input type="text" name="name"><br>
+        <input type="text" name="name" placeholder="Nombre" value="{{old('name')}}">
+        @if($errors->has('name'))
+            <p>{{$errors->first('name')}}</p>
+        @endif
+        <br>
 
         <label for="email">Email:</label>
-        <input type="email" name="email"><br>
+        <input type="email" name="email" placeholder="Email" value="{{old('email')}}">
+        @if($errors->has('email'))
+            <p>{{$errors->first('email')}}</p>
+        @endif
+        <br>
 
         <label for="password">Contraseña:</label>
-        <input type="password" name="password"><br>
+        <input type="password" name="password" placeholder="Contraseña">
+        @if($errors->has('password'))
+            <p>{{$errors->first('password')}}</p>
+        @endif
+        <br>
 
 
         <button type="submit">Crear usuario</button>
